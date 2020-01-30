4d2467e6f439372d804d3c8b948075f7c32942a7
Niteshsharma17021997 <60471265+Niteshsharma17021997@users.noreply.github.com>
2020-01-30 19:00:58 +0530

    Project Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using School.Models;
+
+namespace School.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Role = "Guest";
+            ViewBag.login = true;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Role = "Guest";
+            ViewBag.login = true;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Role = "Guest";
+            ViewBag.login = true;
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            ViewBag.Role = "Admin";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace School.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            ViewBag.Role = "Guest";
+            return View();
+        }
+        public IActionResult Validate()
+        {
+            if (Request.Form["username"] == "Admin" && Request.Form["password"] == "Admin")
+            {
+                return RedirectToAction("admin", "home");
+            }
+            else
+                return RedirectToAction("login", "login");
+        }
+    }
+}
